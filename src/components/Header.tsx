52d08bcc5940a7c511fe037bb1bae574f3f721dd
Timileyin Okediya <105449068+Awo-Oosha@users.noreply.github.com>
2025-05-11 19:37:55 +0100

    fix: improve header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,41 +15,59 @@ const Header = () => {
   ];
 
   const { isHeroInView } = useController();
+  const [isScrolled, setIsScrolled] = React.useState<boolean>(false);
+
+  React.useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
     <header
       className={cn(
-      "flex z-50 w-screen justify-between items-center fixed top-0 left-0 transition-colors duration-300",
-      {
-        "bg-primary/80 backdrop-blur-md": !isHeroInView,
-      }
+        "flex z-50 w-screen justify-between items-center fixed top-0 left-0 transition-colors duration-300",
+        {
+          "bg-primary/80": !isHeroInView,
+          "backdrop-blur-md": isScrolled,
+        }
       )}
     >
       <Container>
-      <div className="flex w-full justify-between items-center px-20 py-3.5">
-        {/* Logo */}
-        <div className="flex items-center gap-[30px] text-white font-inter text-2xl font-medium leading-[normal]">
-        Logo
+        <div className="flex w-full justify-between items-center px-20 py-3.5">
+          {/* Logo */}
+          <div className="flex items-center gap-[30px] text-white font-inter text-2xl font-medium leading-[normal]">
+            Logo
+          </div>
+
+          {/* Navs */}
+          <nav className="flex items-center gap-[30px]">
+            {navs.map((nav, idx) => (
+              <Link
+                className="text-white font-inter text-base font-normal leading-[normal]"
+                key={idx}
+                href={nav.link}
+              >
+                {nav.name}
+              </Link>
+            ))}
+          </nav>
+
+          {/* Button */}
+
+          <Button className="px-[30px] py-2.5 !bg-white !text-dark">
+            About
+          </Button>
         </div>
-
-        {/* Navs */}
-        <nav className="flex items-center gap-[30px]">
-        {navs.map((nav, idx) => (
-          <Link
-          className="text-white font-inter text-base font-normal leading-[normal]"
-          key={idx}
-          href={nav.link}
-          >
-          {nav.name}
-          </Link>
-        ))}
-        </nav>
-
-        {/* Button */}
-
-        <Button className="px-[30px] py-2.5 !bg-white !text-dark">
-        About
-        </Button>
-      </div>
       </Container>
     </header>
   );
