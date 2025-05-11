9c8076bf8b6ccb6003bf4bea25506c3aa28d3cb3
Timileyin Okediya <105449068+Awo-Oosha@users.noreply.github.com>
2025-05-11 17:32:29 +0100

    feat: footer section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Hero from "@/components/Landing/Hero";
 import Image from "next/image";
@@ -7,6 +8,7 @@ export default function Home() {
     <div className="flex w-full min-h-screen flex-col justify-start items-center gap-20 bg-white">
       <Header />
       <Hero />
+      <Footer />
     </div>
   );
 }
