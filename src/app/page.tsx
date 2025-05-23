65e8a8be4b20c9eda1635aa56993ea15b8ffb1ce
Timileyin Okediya <105449068+Awo-Oosha@users.noreply.github.com>
2025-05-23 16:43:30 +0100

    chores: typography

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex w-full min-h-screen flex-col justify-start items-center gap-20 bg-surface">
-     Landing page
+    <div className="flex  w-full min-h-screen flex-col justify-start items-center gap-20 bg-surface display-large">
+      Landing page
     </div>
   );
 }
