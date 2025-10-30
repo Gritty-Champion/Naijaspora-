e6c9a20b700adb8af5bbf3d80756c2af64600a80
Timileyin Okediya <105449068+Awo-Oosha@users.noreply.github.com>
2025-10-30 14:34:06 +0100

    fix

--- config/config.ts
+++ config/config.ts
@@ -10,7 +10,7 @@ const getURL = (): EnvironmentConfig => {
       CURRENT_ENV: "production",
     },
     staging: {
-      API_SERVER_URL: "http://167.99.191.115:3001/api",
+      API_SERVER_URL: "https://167.99.191.115/stage/api",
       CURRENT_ENV: "staging",
     },
     development: {
