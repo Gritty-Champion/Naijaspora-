0aa79fec3cf25073dcac79bef33ba539237d823e
Gritty-Champion <122834528+Gritty-Champion@users.noreply.github.com>
2025-10-30 07:49:49 -0500

    Revert "Revert "Colors""

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,7 @@
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
-import Features from "@/components/Landing/Features";
-import Hero from "@/components/Landing/Hero";
-
 export default function Home() {
   return (
-    <div className="flex w-full min-h-screen flex-col justify-start items-center gap-20 bg-white">
-      <Header />
-      <Hero />
-      <Features />
-      <Footer />
+    <div className="flex  w-full min-h-screen flex-col justify-start items-center gap-20 bg-surface display-large">
+      Landing page
     </div>
   );
 }
